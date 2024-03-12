9cca31e41b29e4b43d89136aa3938f380a8e6afb
Tom Lienard <tom.lienrd@gmail.com>
2024-03-12 11:01:31 +0000

    refactor: remove assets directory

--- src/server/api/runtime-keys.get.ts
+++ src/server/api/runtime-keys.get.ts
@@ -1,5 +1,7 @@
 import data from "runtime-compat-data"
 
 export default defineEventHandler(() => {
+  // Arbritary use an API to retrieve the `support` field and as such,
+  // retrieve the list of runtimes.
   return Object.keys(data.api.AbortController.__compat?.support ?? {})
 })
