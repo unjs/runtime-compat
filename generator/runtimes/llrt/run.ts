938214fd5e3011d4bfe25a406c1d360ee6ac1ae6
Matt Kane <matt.kane@netlify.com>
2024-03-13 11:22:17 +0000

    chore: add icon

--- generator/runtimes/llrt/run.ts
+++ generator/runtimes/llrt/run.ts
@@ -2,6 +2,6 @@ import { runTests } from "../../shared/test.js";
 import tests from "../../../vendor/tests.json";
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
-runTests(tests, undefined, Boolean(process.env.DEBUG)).then((data) => {
+runTests(tests).then((data) => {
   console.log(JSON.stringify(data, undefined, 2));
 });
