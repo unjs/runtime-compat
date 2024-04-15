03daa95f250b147f1407b6fe0c96c4d8bbe33601
Matt Kane <m@mk.gg>
2024-04-15 20:41:03 +0100

    fix: strip script output before logged JSON (#113)

--- generator/runtimes/node/run.ts
+++ generator/runtimes/node/run.ts
@@ -2,4 +2,5 @@ import { runTests } from "../../shared/test.js";
 import tests from "../../../vendor/tests.json";
 
 const data = await runTests(tests);
+console.log("RUNTIME_DATA_START");
 console.log(JSON.stringify(data, undefined, 2));
