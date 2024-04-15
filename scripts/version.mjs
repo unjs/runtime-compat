813a348d7dbb5b6cbc394c5c5e91403d7322f40a
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-04-14 22:17:34 -0700

    Ensure proper line is outputted in versions script

--- scripts/version.mjs
+++ scripts/version.mjs
@@ -4,7 +4,7 @@ import { readdir } from "node:fs/promises";
 const runtimeDir = new URL("../generator/runtimes", import.meta.url);
 const runtimes = await readdir(runtimeDir);
 
-console.log("Runtime Versions:");
+console.log("versions<<EOF");
 
 for (const runtime of runtimes) {
   const { default: data } = await import(`../generator/runtimes/${runtime}/data.json`, {
